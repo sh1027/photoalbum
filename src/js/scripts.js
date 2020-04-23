658fbfbd56bc5a43513da909fadca4512879e07f
sh1027 <shiowill.1027reg@gmail.com>
2020-04-23 10:07:51 +0900

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,7 +5,7 @@ $(function(){
    function loaderClose(){
      $("#loading").fadeOut('slow');
    }
-   setTimeout(loaderClose,10000);
+   setTimeout(loaderClose,2000);
 });
 //
 // let vh = window.innerHeight * 0.01;
